ed715dcc38f94d688054591d6c2eb83956aedf71
ChungIl <cnddlf12@naver.com>
2023-08-24 22:01:01 +0900

    Initial commit

---
+++ src/main/java/com/kimch321/apiretemfo/ApiRetemfoApplication.java
@@ -0,0 +1,13 @@
+package com.kimch321.apiretemfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRetemfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRetemfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kimch321/apiretemfo/ApiRetemfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimch321.apiretemfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRetemfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
